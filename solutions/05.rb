764e0991b737710c2497e9fb0fa1b082904a24dd
Maria Rangelova <maria.rangelova93@gmail.com>
2016-01-26 21:49:48 +0200

    Update task 5 solution
    * Introduce three classes: Stage, Success, Failure
    * Update Commit class to include a parent commit

--- solutions/05.rb
+++ solutions/05.rb
@@ -1,121 +1,28 @@
 require 'digest/sha1'
 
 class ObjectStore
-  def self.init(&block)
-    repo = new
-    repo.instance_eval &block if block_given?
-    repo
-  end
+  class Stage
+    attr_reader :added_objects, :removed_objects
 
-  def initialize
-    @stage = {}
-    @branch_manager = BranchManager.new
-  end
-
-  def add(name, object)
-    @stage[name] = object
-    Status.new(message: "Added #{name} to stage.",
-               success: true,
-               result: object)
-  end
-
-  def remove(name)
-    if @stage.include? name
-      Status.new(message: "Added #{name} for removal.",
-                 success: true,
-                 result: @stage.delete(name))
-    else
-      Status.new(message: "Object #{name} is not committed.",
-                 success: false)
+    def initialize
+      @added_objects = {}
+      @removed_objects = []
     end
-  end
 
-  def commit(message)
-    if number_of_objects_changed == 0
-      Status.new(message: "Nothing to commit, working directory clean.",
-                 success: false)
-    else
-      objects_changed = number_of_objects_changed
-      current_branch.commits << Commit.new(message, @stage.dup)
-      Status.new(message: "#{message}\n\t#{objects_changed} objects changed",
-                 success: true,
-                 result: last_commit)
+    def add(object_name, object)
+      added_objects[object_name] = object
     end
-  end
 
-  def number_of_objects_changed
-    return @stage.size unless last_commit
-
-    objects_added = (@stage.values - last_commit.objects).size
-    objects_removed = (last_commit.objects - @stage.values).size
-
-    objects_added + objects_removed
-  end
-
-  def last_commit
-    current_branch.commits.last
-  end
-
-  def head
-    if last_commit
-      Status.new(message: "#{last_commit.message}",
-                 success: true,
-                 result: last_commit)
-    else
-      Status.new(message: "Branch #{branch.current_branch.name} " \
-                          "does not have any commits yet.",
-                 success: false)
+    def remove(object_name)
+      removed_objects << object_name
     end
-  end
 
-  def log
-    if current_branch.commits.empty?
-      Status.new(message: "Branch #{branch.current_branch.name} " \
-                          "does not have any commits yet.",
-                 success: false)
-    else
-      message = current_branch.commits.reverse.map do |commit|
-        "Commit #{commit.hash}\n" \
-        "Date: #{commit.date.strftime("%a %b %d %H:%M %Y %z")}" \
-        "\n\n\t#{commit.message}"
-      end
-      Status.new(message: message.join("\n\n"),
-                 success: true)
+    def empty?
+      added_objects.empty? and removed_objects.empty?
     end
-  end
 
-  def checkout(sha1)
-    if current_branch.commits.none? { |commit| commit.hash == sha1 }
-      Status.new(message: "Commit #{sha1} does not exist.",
-                 success: false)
-    else
-      commit_index =
-        current_branch.commits.find_index { |commit| commit.hash == sha1 }
-      current_branch.commits = current_branch.commits[0..commit_index]
-      @stage = last_commit.objects_with_names
-
-      Status.new(message: "HEAD is now at #{sha1}.",
-                 success: true,
-                 result: last_commit)
-    end
-  end
-
-  def branch
-    @branch_manager
-  end
-
-  def current_branch
-    branch.current_branch
-  end
-
-  def get(name)
-    if last_commit and last_commit.objects_with_names.include?(name)
-      Status.new(message: "Found object #{name}.",
-                 success: true,
-                 result: last_commit.objects_with_names[name])
-    else
-      Status.new(message: "Object #{name} is not committed.",
-                 success: false)
+    def size
+      added_objects.size + removed_objects.size
     end
   end
 
@@ -123,103 +30,246 @@ class ObjectStore
     attr_accessor :current_branch, :branches
 
     def initialize
-      @current_branch = Branch.new(name: 'master')
+      @current_branch = Branch.new('master')
       @branches = [@current_branch]
     end
 
     def create(name)
       if branch_exists?(name)
-        Status.new(message: "Branch #{name} already exists.",
-                   success: false)
+        Failure.new("Branch #{name} already exists.")
       else
-        branches << Branch.new(name: name, commits: current_branch.commits)
-        Status.new(message: "Created branch #{name}.",
-                   success: true)
+        branches << Branch.new(name, current_branch.commits.dup)
+
+        Success.new("Created branch #{name}.")
       end
     end
 
-    def remove(name)
-      if branch_exists?(name) and current_branch?(name)
-        Status.new(message: 'Cannot remove current branch.',
-                   success: false)
-      elsif branch_exists?(name) and not current_branch?(name)
-        branches.delete_if { |branch| branch.name == name }
-        Status.new(message: "Removed branch #{name}.",
-                   success: true)
-      elsif not branch_exists?(name)
-        Status.new(message: "Branch #{name} does not exist.",
-                   success: false)
+    def remove(branch_name)
+      if branch_exists?(branch_name) and current_branch?(branch_name)
+        Failure.new('Cannot remove current branch.')
+      elsif branch_exists?(branch_name) and not current_branch?(branch_name)
+        branches.delete_if { |branch| branch.name == branch_name }
+
+        Success.new("Removed branch #{branch_name}.")
+      elsif not branch_exists?(branch_name)
+        Failure.new("Branch #{branch_name} does not exist.")
       end
     end
 
-    def checkout(name)
-      if branch_exists?(name)
-        current_branch = branches.find { |branch| branch.name == name }
-        unless current_branch.commits.empty?
-          @stage = current_branch.commits.last.objects_with_names
-        end
-        Status.new(message: "Switched to branch #{name}.",
-                   success: true)
+    def checkout(branch_name)
+      if branch_exists?(branch_name)
+        @current_branch = branches.find { |branch| branch.name == branch_name }
+
+        Success.new("Switched to branch #{branch_name}.")
       else
-        Status.new(message: "Branch #{name} does not exist.",
-                   success: false)
+        Failure.new("Branch #{branch_name} does not exist.")
       end
     end
 
     def list
-      message = branches.map(&:name).sort.map do |name|
-        current_branch?(name) ? "* #{name}" : "  #{name}"
-      end
-      Status.new(message: message.join("\n"),
-                 success: true)
+      message = branches.map(&:name).sort.map do |branch_name|
+        current_branch?(branch_name) ? "* #{branch_name}" : "  #{branch_name}"
+      end.join("\n")
+
+      Success.new(message)
     end
 
-    def branch_exists?(name)
-      branches.any? { |branch| branch.name == name}
+    def branch_exists?(branch_name)
+      branches.any? { |branch| branch.name == branch_name}
     end
 
-    def current_branch?(name)
-      current_branch.name == name
+    def current_branch?(branch_name)
+      current_branch.name == branch_name
     end
 
     class Branch
       attr_reader :name
       attr_accessor :commits
 
-      def initialize(name:, commits: [])
+      def initialize(name, commits = [])
         @name = name
         @commits = commits
+      end
+
+      def commit(commit)
+        @commits << commit
       end
     end
   end
 
-  class Status
-    attr_reader :message, :success, :result
+  class Commit
+    attr_reader :message, :parent, :stage, :date, :hash
 
-    def initialize(message:, success: , result: nil)
+    def initialize(message, parent, stage)
       @message = message
-      @success = success
-      @result = result
+      @parent = parent
+      @stage = stage
+      @date = Time.now
+      @hash = Digest::SHA1.hexdigest "#{formatted_date}#{message}"
     end
 
-    def success?
-      success
+    def objects
+      merged_stage.values
     end
 
-    def error?
-      not success?
+    def object(object_name)
+      merged_stage[object_name]
+    end
+
+    def has_object?(object_name)
+      merged_stage.include?(object_name)
+    end
+
+    def to_s
+      "Commit #{hash}\nDate: #{formatted_date}\n\n\t#{message}"
+    end
+
+    protected
+
+    def merged_stage
+      return stage.added_objects unless parent
+
+      parent.merged_stage.merge(stage.added_objects).reject do |name, object|
+        stage.removed_objects.include?(name)
+      end
+    end
+
+    private
+
+    def formatted_date
+      @date.strftime("%a %b %d %H:%M %Y %z")
     end
   end
 
-  class Commit
-    attr_reader :message, :objects, :objects_with_names, :date, :hash
+  class Status
+    attr_reader :message, :result
 
-    def initialize(message, objects)
+    def initialize(message, result = nil)
       @message = message
-      @objects = objects.values
-      @objects_with_names = objects
-      @date = Time.now
-      @hash = Digest::SHA1.hexdigest "#{@time}#{message}"
+      @result = result
     end
+  end
+
+  class Success < Status
+    def success?
+      true
+    end
+
+    def error?
+      false
+    end
+  end
+
+  class Failure < Status
+    def success?
+      false
+    end
+
+    def error?
+      true
+    end
+  end
+
+  attr_reader :stage, :branch_manager
+
+  def self.init(&block)
+    repository = new
+
+    repository.instance_eval &block if block_given?
+
+    repository
+  end
+
+  def initialize
+    @stage = Stage.new
+    @branch_manager = BranchManager.new
+  end
+
+  def add(object_name, object)
+
+    stage.add(object_name, object)
+    Success.new("Added #{object_name} to stage.", object)
+  end
+
+  def remove(object_name)
+    if last_commit.has_object? object_name
+      stage.remove(object_name)
+
+      Success.new("Added #{object_name} for removal.",
+                  last_commit.object(object_name))
+    else
+      Failure.new("Object #{object_name} is not committed.")
+    end
+  end
+
+  def commit(message)
+    if stage.empty?
+      Failure.new("Nothing to commit, working directory clean.")
+    else
+      current_stage = stage.dup
+      current_branch.commit(Commit.new(message, last_commit, current_stage))
+      @stage = Stage.new
+
+      Success.new("#{message}\n\t#{current_stage.size} objects changed",
+                  last_commit)
+    end
+  end
+
+  def head
+    if last_commit
+      Success.new("#{last_commit.message}", last_commit)
+    else
+      Failure.new("Branch #{current_branch.name} does " \
+                  "not have any commits yet.")
+    end
+  end
+
+  def log
+    if current_branch.commits.empty?
+      Failure.new("Branch #{current_branch.name} does " \
+                  "not have any commits yet.")
+    else
+      message = current_branch.commits.reverse.map(&:to_s)
+      Success.new(message.join("\n\n"))
+    end
+  end
+
+  def checkout(sha1)
+    if current_branch.commits.none? { |commit| commit.hash == sha1 }
+      Failure.new("Commit #{sha1} does not exist.")
+    else
+      commit_index = current_branch.commits.find_index do |commit|
+        commit.hash == sha1
+      end
+
+      current_branch.commits = current_branch.commits[0..commit_index]
+
+      @stage = last_commit.objects
+
+      Success.new("HEAD is now at #{sha1}.", last_commit)
+    end
+  end
+
+  def get(object_name)
+    if last_commit and last_commit.has_object? object_name
+      Success.new("Found object #{object_name}.",
+                  last_commit.object(object_name))
+    else
+      Failure.new("Object #{object_name} is not committed.")
+    end
+  end
+
+  def branch
+    branch_manager
+  end
+
+  private
+
+  def last_commit
+    current_branch.commits.last
+  end
+
+  def current_branch
+    branch_manager.current_branch
   end
 end
